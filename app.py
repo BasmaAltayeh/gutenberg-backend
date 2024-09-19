24d354f0d6abc3f2526d9ab8843dfbffb9872564
BasmaAltayeh <basma.altayeh@gmail.com>
2024-09-19 14:03:54 +0300

    Initial commit

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask_cors import CORS
+from routes.book_routes import book_routes
+from routes.analysis_routes import analysis_routes
+
+
+app = Flask(__name__)
+
+CORS(app, resources={r"/*": {"origins": "*"}})
+
+# Register blueprints (routes)
+app.register_blueprint(book_routes)
+app.register_blueprint(analysis_routes)
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5002)
